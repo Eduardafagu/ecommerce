5dac49c563207a33e4930cfbd9b6ef223b869616
Maria Eduarda Fagundes <eduarda2fagundes@gmail.com>
2022-06-18 11:36:29 -0300

    primeira etapa

---
+++ Js/products-cards.js
@@ -0,0 +1,42 @@
+function initProductsCard(selector, products) {
+    const parent = document.getElementById(selector)
+    parent.innerHTML = ''
+
+
+    for (let i = 0; i < products.length; i +=1) {
+        const product = products [i];
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = `/images/${product.img_url}`;
+        img.setAttribute("alt", product.name);
+        figure.append(img);
+        card.append(figure);
+
+        const info = document.createElement('div')
+        info.className ='info'
+        const name = document.createElement('header')
+        name.innerText = product.name
+        const detail = document.createElement('div')
+        detail.className = 'detail'
+        detail.innerText = product.detail
+        const price = document.createElement('div')
+        price.className = 'price'
+        price.innerText = format(product.price)
+        detail.innerText = product.detail
+        const footer = document.createElement('footer')
+        const remove = document.createElement('button')
+        remove.innerText = 'Adcionar'
+
+        footer.append(remove)
+        info.append(name, detail, price, footer)
+        card.append(info);
+
+
+        parent.append(card)
+    }
+}
+
+initProductsCard('products-cards', products)
---
+++ Js/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    img_url: "products/base.webp",
+    name: "Produto Base",
+    detail: "Base iluminadora",
+    price: 29.5,
+  },
+  {
+    id: 2,
+    img_url: "products/batom.jpg",
+    name: "Produto Batom Mac",
+    detail: "Batom hidratante",
+    price: 60.5,
+  },
+  {
+    id: 3,
+    img_url: "products/rimelmac.jpg",
+    name: "Produto Rimel Mac",
+    detail: "Rimel",
+    price: 55.5,
+  },
+
+    {
+    id: 4,
+    img_url: "products/glitermac.jpg",
+    name: "Gliter Mac",
+    detail: "Gliter MAC",
+    price: 40.5,
+},
+];
