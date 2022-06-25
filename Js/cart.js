a9521ff9702fc358960acf67ccb9f6162e3a96bb
Maria Eduarda Fagundes <eduarda2fagundes@gmail.com>
2022-06-25 12:12:02 -0300

    ultimaaula

--- Js/cart.js
+++ Js/cart.js
@@ -1,22 +1,46 @@
 class Cart {
-    constructor() {
-    this.list = [];
+  constructor() {
     this.badge = document.getElementById("cart-badge");
+    this.list = [];
+
+    if (!!localStorage.cart) {
+      this.list = JSON.parse(localStorage.getItem("cart"));
+      this.reloadBadge();
     }
+  }
 
-addProduct(product) {
-this.list.push(product);
-this.badge.innerText = this.list.length;
+  getList() {
+    return this.list;
+  }
 
-if (this.list.length > 0) {
-    this.badge.style.display = 'flex';
-} else {
-    this.badge.style.display = "none";
-}
-}
-getAmountProducts() {
-  return this.list.length;
-}
+  reloadBadge() {
+    this.badge.innerText = this.list.length;
+
+    if (this.list.length > 0) {
+      this.badge.style.display = "flex";
+    } else {
+      this.badge.style.display = "none";
+    }
+  }
+
+  addProduct(product) {
+    this.list.push(product);
+    this.reloadBadge();
+    this.updateStorage();
+  }
+
+  removeProduct(product) {
+    const index = this.list.findIndex(function (item) {
+      return item.id === product.id;
+    });
+    this.list.splice(index, 1);
+    this.reloadBadge();
+    this.updateStorage();
+  }
+
+  updateStorage() {
+    localStorage.setItem("cart", JSON.stringify(this.list));
+  }
 }
 
-const cart = new Cart()
+const cart = new Cart();
